acc0ce427c13095b5ae8f21375a5a1bb2e839b89
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2024-10-22 22:27:28 +0900

    [Add]FadeManager作成、ユウアイ特殊攻撃制作中。

--- Assets/Iwadare/Scripts/Enemy/AttackState/YuaiSpecialAttack.cs
+++ Assets/Iwadare/Scripts/Enemy/AttackState/YuaiSpecialAttack.cs
@@ -1,7 +1,9 @@
 ﻿using Cinemachine;
+using DG.Tweening;
 using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class YuaiSpecialAttack : MonoBehaviour, AttackInterface, PauseTimeInterface
 {
@@ -10,11 +12,17 @@ public class YuaiSpecialAttack : MonoBehaviour, AttackInterface, PauseTimeInterf
     [SerializeField] float _moveTime = 3f;
     [SerializeField] float _disAttackTime = 0.1f;
     [SerializeField] float _attackCoolTime = 2f;
+    [SerializeField] Transform _bossPos;
     [SerializeField] Canvas _uiCanvas;
     [SerializeField] Canvas _specialUI;
+    [SerializeField] Image _fadeImage;
+    [SerializeField] Animator _bossFadeAnim;
+    [SerializeField] AnimationClip _bossFadeAnimation;
     [SerializeField] CinemachineVirtualCamera _BossUpCamera;
+    [SerializeField] MimicryPos[] _mimicryPos;
     CinemachineBrain _cameraBrain;
     float _currentTime = 0f;
+    int randomNumber = 0;
 
     public void UnityActionSet()
     {
@@ -34,8 +42,14 @@ public class YuaiSpecialAttack : MonoBehaviour, AttackInterface, PauseTimeInterf
     public void Init()
     {
         if (_specialUI) _specialUI.enabled = false;
-        gameObject.SetActive(false);
+        _bossFadeAnim.gameObject.SetActive(false);
         _cameraBrain = Camera.main.GetComponent<CinemachineBrain>();
+        foreach (var mimic in _mimicryPos)
+        {
+            mimic._hitPosColider.gameObject.SetActive(false);
+            mimic._hitPosImage.enabled = false;
+        }
+        gameObject.SetActive(false);
     }
 
     public void StayUpdate(EnemyBase enemy)
@@ -55,13 +69,21 @@ public class YuaiSpecialAttack : MonoBehaviour, AttackInterface, PauseTimeInterf
 
     public IEnumerator Move(EnemyBase enemy)
     {
+        GameStateManager.Instance.ChangeState(GameState.BattleStopState);
+        enemy.transform.DOMove(_bossPos.position,_moveTime);
         if (_uiCanvas) _uiCanvas.enabled = false;
         if (_BossUpCamera) _BossUpCamera.Priority = 20;
         if (_specialUI) _specialUI.enabled = true;
-        yield return new WaitForSeconds(2f);
+        yield return new WaitForSeconds(_moveTime);
         if (_BossUpCamera) _BossUpCamera.Priority = 0;
         if (_uiCanvas) _uiCanvas.enabled = true;
         if (_specialUI) _specialUI.enabled = false;
+        yield return new WaitForSeconds(1f);
+        _bossFadeAnim.gameObject.SetActive(true);
+        yield return StartCoroutine(FadeManager.Instance.CustomFadeIn(_fadeImage,0.5f));
+        _bossFadeAnim.Play(_bossFadeAnimation.name);
+        yield return StartCoroutine(FadeManager.Instance.FadeIn());
+
         yield return null;
     }
 
@@ -73,6 +95,12 @@ public class YuaiSpecialAttack : MonoBehaviour, AttackInterface, PauseTimeInterf
     public void ActionReset(EnemyBase enemy)
     {
         throw new System.NotImplementedException();
+    }
+
+    public int RandomNumber(int max)
+    {
+        UnityEngine.Random.InitState(DateTime.Now.Millisecond);
+        return UnityEngine.Random.Range(0, max);
     }
 
     public void TimeScaleChange(float timeScale)
@@ -88,5 +116,12 @@ public class YuaiSpecialAttack : MonoBehaviour, AttackInterface, PauseTimeInterf
     public void EndPause()
     {
         throw new NotImplementedException();
+    }
+
+    [Serializable]
+    struct MimicryPos
+    {
+        public Collider2D _hitPosColider;
+        public Text _hitPosImage;
     }
 }
---
+++ Assets/Iwadare/Scripts/Manager/FadeManager.cs
@@ -0,0 +1,108 @@
+﻿using DG.Tweening;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using UnityEngine.UI;
+
+public class FadeManager : SingletonMonovihair<FadeManager>,PauseTimeInterface
+{
+    [SerializeField] Image _defaultFadeImage;
+    [SerializeField] float _fadeInTime = 1f;
+    [SerializeField] float _fadeOutTime = 1f;
+    Sequence _fadeSequence;
+    Tweener _currrentFadeTween;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        _fadeSequence = DOTween.Sequence();
+        var color = _defaultFadeImage.color;
+        color.a = 0;
+        _defaultFadeImage.color = color;
+        _fadeSequence
+            .Append(_defaultFadeImage.DOFade(1f, _fadeInTime))
+            .Append(_defaultFadeImage.DOFade(0f, _fadeOutTime));
+        _fadeSequence.Pause();
+    }
+
+    private void OnEnable()
+    {
+        TimeScaleManager.ChangeTimeScaleAction += TimeScaleChange;
+        TimeScaleManager.StartPauseAction += StartPause;
+        TimeScaleManager.EndPauseAction += EndPause;
+    }
+
+    private void OnDisable()
+    {
+        TimeScaleManager.ChangeTimeScaleAction -= TimeScaleChange;
+        TimeScaleManager.StartPauseAction -= StartPause;
+        TimeScaleManager.EndPauseAction -= EndPause;
+    }
+
+    public IEnumerator SceneChangeFade()
+    {
+        yield return _defaultFadeImage.DOFade(1f,_fadeInTime);
+        //ロード処理完了的な何か
+        yield return _defaultFadeImage.DOFade(0f,_fadeOutTime);
+    }
+
+    public IEnumerator Fade()
+    {
+        Debug.Log("fade開始");
+        yield return _fadeSequence.Play();
+        yield return new WaitForSeconds(_fadeInTime + _fadeOutTime);
+    }
+
+    public IEnumerator FadeIn()
+    {
+        _currrentFadeTween = _defaultFadeImage.DOFade(1f, _fadeInTime);
+        yield return _currrentFadeTween.Play();
+        yield return new WaitForSeconds(_fadeInTime);
+    }
+
+    public IEnumerator FadeOut()
+    {
+        _currrentFadeTween = _defaultFadeImage.DOFade(0f, _fadeOutTime);
+        _currrentFadeTween.Play();
+        yield return new WaitForSeconds(_fadeOutTime);
+    }
+
+    public IEnumerator CustomFadeIn(Image image,float fadeInTime)
+    {
+        _currrentFadeTween = image.DOFade(1f, fadeInTime);
+        _currrentFadeTween.Play();
+        yield return new WaitForSeconds(fadeInTime);
+    }
+
+    public IEnumerator CustomFadeOut(Image image,float fadeOutTime)
+    {
+        _currrentFadeTween = image.DOFade(0f, fadeOutTime);
+        _currrentFadeTween.Play();
+        yield return new WaitForSeconds(fadeOutTime);
+    }
+
+    public void TimeScaleChange(float timeScale)
+    {
+        if(_currrentFadeTween.IsActive())
+        {
+            _currrentFadeTween.timeScale = timeScale;
+        }
+    }
+
+    public void StartPause()
+    {
+        if (_currrentFadeTween.IsActive())
+        {
+            _currrentFadeTween.timeScale = 0f;
+        }
+    }
+
+    public void EndPause()
+    {
+        if (_currrentFadeTween.IsActive())
+        {
+            _currrentFadeTween.timeScale = 1f;
+        }
+    }
+}
