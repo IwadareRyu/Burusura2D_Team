0ea659ad284d81cd7dd4c69d4f299c845ccf3124
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2024-10-23 20:21:45 +0900

    [Add]ユウアイ特殊攻撃作成中

--- Assets/Iwadare/Scripts/Enemy/AttackState/Yuai/YuaiActions.cs
+++ Assets/Iwadare/Scripts/Enemy/AttackState/Yuai/YuaiActions.cs
@@ -6,5 +6,5 @@ public class YuaiActions
     public DashAttack _dashAttack = new();
     public SampleAttack _at2 = new();
     public DropKunai _dropKunai;
-    public YuaiSpecialAttack _yuaiSpecialAttack = new();
+    public YuaiSpecialAttack_Main _yuaiSpecialAttack = new();
 }
