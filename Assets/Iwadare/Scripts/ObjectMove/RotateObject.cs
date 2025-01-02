7d1f7436d3a8ab3d0ff7f28fdec04e89e36a9d06
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2025-01-03 00:52:48 +0900

    [Add]イラスト本描きに差し替え、Spawn予知の表現変更

---
+++ Assets/Iwadare/Scripts/ObjectMove/RotateObject.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateObject : MonoBehaviour,PauseTimeInterface
+{
+    [SerializeField] float _rotateSpeed = 1f;
+    [SerializeField] float _memoRotate;
+
+    private void Awake()
+    {
+        _memoRotate = _rotateSpeed;
+    }
+
+    private void OnEnable()
+    {
+        TimeScaleManager.ChangeTimeScaleAction += TimeScaleChange;
+        TimeScaleManager.StartPauseAction += StartPause;
+        TimeScaleManager.EndPauseAction += EndPause;
+    }
+
+    private void OnDisable()
+    {
+        TimeScaleManager.ChangeTimeScaleAction -= TimeScaleChange;
+        TimeScaleManager.StartPauseAction -= StartPause;
+        TimeScaleManager.EndPauseAction -= EndPause;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Vector3.forward * _rotateSpeed);
+    }
+
+    public void EndPause()
+    {
+        _rotateSpeed = _memoRotate;
+    }
+
+    public void StartPause()
+    {
+        _rotateSpeed = 0f;
+    }
+
+    public void TimeScaleChange(float timeScale)
+    {
+        _rotateSpeed = _memoRotate * timeScale;
+    }
+
+}
