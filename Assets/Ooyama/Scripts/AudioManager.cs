e3ee70b81ae9a45be100795bc1055f276f53dc38
OoyamaShun130724 <shun20010724@icloud.com>
2025-01-07 22:00:55 +0900

    AudioManager仮作成

---
+++ Assets/Ooyama/Scripts/AudioManager.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    //ファイル内のSE数
+    private int _seCount = 5;
+
+    //音量設定保存用のKey及び各音声のデフォルト音量
+    private const string BGM_VOLUME_KEY = "BGM_VOLUME";
+    private const string SE_VOLUME_KEY = "SE_VOLUME";
+    private const float BGM_VOLUME_DEFULT = 1.0f;
+    private const float SE_VOLUME_DEFULT = 1.0f;
+
+    //各ファイルのパス
+    private const string BGM_PATH = "Audio/BGM";
+    private const string SE_PATH = "Audio/SE";
+
+    //各AudioSource
+    private AudioSource _bgmSource;
+    private List<AudioSource> _seSourcesLis;
+
+    //ファイル内を名称検索するためのDictionary
+    private Dictionary<string, AudioClip> _bgmDic;
+    private Dictionary<string, AudioClip> _seDic;
+
+    public static AudioManager Instance = null;
+
+    private AudioMixer _audioMixer = null;
+
+    private void Awake()
+    {
+
+        if (Instance == null) Instance = this;
+        else Destroy(this);
+
+        DontDestroyOnLoad(Instance.gameObject);
+
+        _bgmDic = new();
+        _seDic = new();
+
+        var bgmList = Resources.LoadAll(BGM_PATH);
+        foreach (AudioClip bgm in bgmList)
+        {
+            _bgmDic[bgm.name] = bgm;
+        }
+
+        var seList = Resources.LoadAll(SE_PATH);
+        foreach (AudioClip se in seList)
+        {
+            _seDic[se.name] = se;
+        }
+
+        _seCount = seList.Length;
+
+        gameObject.AddComponent<AudioListener>();
+        for (int i = 0; i <= _seCount; i++)
+        {
+            gameObject.AddComponent<AudioSource>();
+        }
+
+        AudioSource[] audioSources = GetComponents<AudioSource>();
+        _seSourcesLis = new();
+
+        //audioSources[0].loop = true;
+
+
+        for (int i = 0; i < audioSources.Length; i++)
+        {
+            audioSources[i].playOnAwake = false;
+            if (i == 0)
+            {
+                audioSources[i].loop = true;
+                _bgmSource = audioSources[i];
+                _bgmSource.volume = GetBGMVolume();
+                _bgmSource.outputAudioMixerGroup = _audioMixer.FindMatchingGroups("BGM")[0];
+            }
+            else
+            {
+                _seSourcesLis.Add(audioSources[i]);
+                audioSources[i].volume = GetSEVolume();
+                
+            }
+        }
+
+        //_audioMixer = (AudioMixer)Resources.Load("Mixer");
+        //_audioMixer.outputAudioMixerGroup
+    }
+    public void ChangeVolume(float BGMVolume, float SEVolume)
+    {
+        _bgmSource.volume = BGMVolume;
+        foreach (AudioSource source in _seSourcesLis)
+        {
+            source.volume = SEVolume;
+        }
+
+        SetBGMVolume(BGMVolume);
+        SetSEVolume(SEVolume);
+    }
+    public void PlayBGM(string bgmName)
+    {
+        if (!_bgmDic.ContainsKey(bgmName))
+        {
+            Debug.Log(bgmName + "が存在しません");
+            return;
+        }
+
+        if (!_bgmSource.isPlaying)
+        {
+            _bgmSource.clip = _bgmDic[bgmName];
+            _bgmSource.Play();
+        }
+        else
+        {
+            StopBGM();
+            PlayBGM(bgmName);
+        }
+    }
+    public void PlaySE(string seName)
+    {
+        if (!_seDic.ContainsKey(seName))
+        {
+            Debug.Log(seName + "が存在しません");
+            return;
+        }
+
+        foreach (AudioSource source in _seSourcesLis)
+        {
+
+            Debug.Log("Play" + seName);
+            source.PlayOneShot(_seDic[seName]);
+            return;
+        }
+    }
+    public void TestBGM()
+    {
+        PlayBGM("Test");
+    }
+    public void TestSE()
+    {
+        PlaySE("GetItem");
+    }
+    public void TestSE2()
+    {
+        PlaySE("se_powerup");
+    }
+    public void StopBGM()
+    {
+        _bgmSource.Stop();
+    }
+    public float GetBGMVolume()
+    {
+        return PlayerPrefs.GetFloat(BGM_VOLUME_KEY, BGM_VOLUME_DEFULT);
+    }
+    public float GetSEVolume()
+    {
+        return PlayerPrefs.GetFloat(SE_VOLUME_KEY, SE_VOLUME_DEFULT);
+    }
+    public void SetBGMVolume(float BGMVolume)
+    {
+        PlayerPrefs.SetFloat(BGM_VOLUME_KEY, BGMVolume);
+    }
+    public void SetSEVolume(float SEVolume)
+    {
+        PlayerPrefs.SetFloat(SE_VOLUME_KEY, SEVolume);
+    }
+}
