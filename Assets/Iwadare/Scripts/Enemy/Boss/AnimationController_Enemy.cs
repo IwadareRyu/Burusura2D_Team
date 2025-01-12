c12e3f568cf188d52217cc9970785922906395e3
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2025-01-12 22:13:08 +0900

    UI枠、Spine系のイラストあげ、Spine調整。

--- Assets/Iwadare/Scripts/Enemy/Boss/AnimationController_Enemy.cs
+++ Assets/Iwadare/Scripts/Enemy/Boss/AnimationController_Enemy.cs
@@ -1,8 +1,39 @@
-﻿using System.Collections;
+﻿using Spine.Unity;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[Serializable]
 public class AnimationController_Enemy : MonoBehaviour
 {
+    public AnimationType _animType;
     public Animator _objAnimator;
+    public Spine.AnimationState _spineAnimationState;
+    public Spine.Skeleton _skeleton;
+    public SkeletonAnimation skeletonAnimation;
+    [SpineAnimation]
+    public string _idleAnimationName; 
+    [SpineAnimation]
+    public string _changeAnimationName; 
+    [SpineAnimation]
+    public string _runAnimationName;
+    [SpineAnimation]
+    public string _damageAnimationName;
+
+    public void Awake()
+    {
+        if (_animType == AnimationType.SkeletonAnimator)
+        {
+            _spineAnimationState = skeletonAnimation.AnimationState;
+            _skeleton = skeletonAnimation.Skeleton;
+        }
+        gameObject.SetActive(false);
+    }
+}
+
+public enum AnimationType
+{
+    NormalAnimator,
+    SkeletonAnimator,
 }
