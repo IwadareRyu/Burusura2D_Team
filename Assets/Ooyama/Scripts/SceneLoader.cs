3ae4d9ba9a8a7ed1e094a7a60c6428078325291c
OoyamaShun130724 <shun20010724@icloud.com>
2025-01-20 21:03:32 +0900

    各マネージャープレハブ化

---
+++ Assets/Ooyama/Scripts/SceneLoader.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader Instance;
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+    public void SceneLoad(string SceneName,float WaitTime)
+    {
+        StartCoroutine(WaitTimer(SceneName,WaitTime));
+    }
+    IEnumerator WaitTimer(string NextSceneName,float WaitTime)
+    {
+        var async = SceneManager.LoadSceneAsync(NextSceneName);
+        async.allowSceneActivation = false;
+        yield return new WaitForSeconds(WaitTime);
+        async.allowSceneActivation = true;
+    }
+    public void Test()
+    {
+        SceneLoad("YuaiScene", 0.7f);
+    }
+}
