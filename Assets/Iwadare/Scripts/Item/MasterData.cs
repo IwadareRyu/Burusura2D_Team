e44128b6956ff70ab32dabfcdf6ab0362e2df7c4
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2025-03-20 03:24:47 +0900

    [Add]GUIからURLを記入し、ロードすることでScriptableObjectを動的生成してデータを入れる機能を作った。

---
+++ Assets/Iwadare/Scripts/Item/MasterData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace MasterDataClass
+{
+    [Serializable]
+    public class ItemData
+    {
+        public int ID;
+        public string ItemName;
+        public string Description;
+        public int BaseAttack;
+        public int MaxAttack;
+        public int BaseDiffence;
+        public int MaxDiffence;
+        public int BaseHP;
+        public int MaxHP;
+    }
+
+    [Serializable]
+    public class MasterDataClass<T>
+    {
+        public string Version;
+        public T[] Data;
+    }
+}
