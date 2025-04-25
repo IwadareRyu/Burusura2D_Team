dba028a7fac3159f78b1c81b4cc153b33073ac03
OoyamaShun130724 <shun20010724@icloud.com>
2025-04-25 22:23:32 +0900

    入力装置の切り替え機能

--- Assets/Ooyama/Scripts/CursorSwitcher.cs
+++ Assets/Ooyama/Scripts/CursorSwitcher.cs
@@ -3,25 +3,28 @@ using UnityEngine.InputSystem;
 
 public class CursorSwitcher : MonoBehaviour
 {
+    public PlayerInput playerInput;
     private string _lastControlScheme = "";
     [SerializeField] private UnityEngine.EventSystems.EventSystem _eventSystem;
     [SerializeField] private TitleController _titleController;
 
     private void Start()
     {
+        playerInput = new();
         if (_titleController == null) _titleController = FindAnyObjectByType<TitleController>();
+        //_lastControlScheme = playerInput.controlSchemes[0].name;
     }
     void Update()
     {
         if (Gamepad.current != null && Gamepad.current.wasUpdatedThisFrame)
         {
             SetCursorVisible(false);
-            _lastControlScheme = "Gamepad";
+            //_lastControlScheme = "Gamepad";
         }
         else if (Mouse.current != null && Mouse.current.wasUpdatedThisFrame)
         {
             SetCursorVisible(true);
-            _lastControlScheme = "Mouse";
+            //_lastControlScheme = "Mouse";
         }
     }
 
