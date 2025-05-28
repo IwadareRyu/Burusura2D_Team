fcbbb2f4737d58f1ae0dd09f7dfef7d3620f5e5f
イワダレリュウゴ <106290660+IwadareRyu@users.noreply.github.com>
2025-05-28 23:07:44 +0900

    [Add]装備しているアイテムの仕様変更、ステータスクラスの仮追加

---
+++ Assets/Iwadare/Scripts/Player/PlayerStatusClass.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
+using UnityEngine;
+
+public class PlayerStatusClass : MonoBehaviour
+{
+    [SerializeField] float _playerDefaultHP = 100f;
+    [SerializeField] int _playerDefaultAttack = 1;
+    [SerializeField] int _playerDefaultDiffence = 1;
+
+    public float _currentHP 
+    { 
+        get { return _currentHP; }
+        set { _currentHP = value; }
+    }
+
+    public int _currentAttack 
+    {
+        get { return _currentAttack; }
+        set { _currentAttack = value; }
+    }
+
+    public int _currentDiffence
+    {
+        get { return _currentDiffence; }
+        set { _currentDiffence = value; }
+    }
+
+
+}
